dd9cbdbb8260156b6f29f4ed000f9c8b6783a2aa
MatthewTighe <matthewdtighe@gmail.com>
2017-11-10 14:21:25 -0800

    Updated move_gen.py and players.py to appease pylint. Removed imcsClient and driver as main functionality is changing to a server.

--- pychess/move_gen.py
+++ pychess/move_gen.py
@@ -1,87 +1,124 @@
+'''
+Generate all possible moves for a position.
+'''
+
+from enum import Enum
+
 import representation
 from representation import Square
 from representation import Move
-from representation import Posn
-from enum import Enum
 
 
 class Capture(Enum):
+    '''
+    Enumeration to represent whether a piece can move in a direction and
+    capture. ONLY shows that pawns can only move diagonally to capture.
+    '''
     YES = True
     NO = False
     ONLY = 2
 
 
-def movescan(x0, y0, dx, dy, posn, capture=True, stop_short=False):
-    x = x0
-    y = y0
+def movescan(x_start, y_start, x_change, y_change, posn, capture=True, stop_short=False):
+    '''
+    Scan for possible moves.
+
+    :param x_start:     x coord to start scan at.
+    :param y_start:     y coord to start scan at.
+    :param x_change:    Rate to move in x direction.
+    :param y_change:    Rate to move in y direction.
+    :param posn:        The board representation to scan.
+    :param capture:     Whether to attempt capture.
+    :param stop_short:  Whether to stop before enemy pieces.
+
+    :returns: All possible moves.
+    '''
+    x_coord = x_start
+    y_coord = y_start
 
     # Check the color of the piece to move
-    c = posn.piece_color(x, y)
+    color = posn.piece_color(x_coord, y_coord)
 
     moves = []
     i = 0
     while True:
-        x = x + dx
-        y = y + dy
-        if x >= representation.NROWS or y >= representation.NCOLS \
-                or x < 0 or y < 0:
+        x_coord = x_coord + x_change
+        y_coord = y_coord + y_change
+        if x_coord >= representation.NROWS or y_coord >= representation.NCOLS \
+                or x_coord < 0 or y_coord < 0:
             break
-        if posn.board[x][y] != '.' :
-            if posn.piece_color(x, y) == c:
+        if posn.board[x_coord][y_coord] != '.':
+            if posn.piece_color(x_coord, y_coord) == color:
                 break
-            if capture == False:
+            if capture is False:
                 break
             stop_short = True
         elif capture == Capture.ONLY:
             break
-        moves.append(Move(Square(x0, y0), Square(x, y)))
+        moves.append(Move(Square(x_start, y_start), Square(x_coord, y_coord)))
         i += 1
-        if posn.board[x][y] == 'k' or posn.board[x][y] == 'K':
+        if posn.board[x_coord][y_coord] == 'k' or posn.board[x_coord][y_coord] == 'K':
             moves[i-1].win = True
         if stop_short:
             break
     return moves
 
 
-def symmscan(x, y, dx, dy, posn, capture=True, stop_short=False):
+def symmscan(x_start, y_start, x_change, y_change, posn, capture=True, stop_short=False):
+    '''
+    Rotate the board and perform move scan for each rotation.
+    See movescan for parameters.
+    '''
     moves = []
-    for i in range(4):
-        moves += movescan(x, y, dx, dy, posn, capture=capture, stop_short=stop_short)
-        dx, dy = dy, dx
-        dx *= -1
+    for _ in range(4):
+        moves += \
+        movescan(x_start, y_start, x_change, y_change, posn, capture=capture, stop_short=stop_short)
+        x_change, y_change = y_change, x_change
+        x_change *= -1
     return moves
 
 
-def movelist(x, y, posn):
-    assert(posn.board[x][y] != '.')
+def movelist(x_start, y_start, posn):
+    '''
+    Generate all possible moves for a piece.
+    See movescan for parameters.
+    '''
+    assert posn.board[x_start][y_start] != '.'
     moves = []
-    kind = posn.kind(x, y)
+    kind = posn.kind(x_start, y_start)
     if kind == 'king' or kind == 'queen':
         stop_short = (kind == 'king')
-        moves += symmscan(x, y, 1, 0, posn, stop_short=stop_short)
-        moves += symmscan(x, y, 1, 1, posn, stop_short=stop_short)
+        moves += symmscan(x_start, y_start, 1, 0, posn, stop_short=stop_short)
+        moves += symmscan(x_start, y_start, 1, 1, posn, stop_short=stop_short)
     elif kind == 'bishop' or kind == 'rook':
         stop_short = (kind == 'bishop')
         capture = (kind == 'rook')
-        moves += symmscan(x, y, 1, 0, posn, capture=capture, stop_short=stop_short)
+        moves += symmscan(x_start, y_start, 1, 0, posn, capture=capture, stop_short=stop_short)
         if kind == 'bishop':
             stop_short = False
             capture = True
-            moves += symmscan(x, y, 1, 1, posn, capture=capture, stop_short=stop_short)
+            moves += symmscan(x_start, y_start, 1, 1, posn, capture=capture, stop_short=stop_short)
     elif kind == 'knight':
-        moves += symmscan(x, y, 2, 1, posn, stop_short=True)
-        moves += symmscan(x, y, 2, -1, posn, stop_short=True)
+        moves += symmscan(x_start, y_start, 2, 1, posn, stop_short=True)
+        moves += symmscan(x_start, y_start, 2, -1, posn, stop_short=True)
     elif kind == 'pawn':
-        d = 1
-        if posn.piece_color(x, y) == 'W':
-            d = -1
-        moves += movescan(x, y, d, -1, posn, capture=Capture.ONLY, stop_short=True)
-        moves += movescan(x, y, d, 1, posn, capture=Capture.ONLY, stop_short=True)
-        moves += movescan(x, y, d, 0, posn, capture=False, stop_short=True)
+        direction = 1 if posn.piece_color(x_start, y_start) == 'W' else -1
+        moves += \
+        movescan(x_start, y_start, direction, -1, posn, capture=Capture.ONLY, stop_short=True)
+        moves += \
+        movescan(x_start, y_start, direction, 1, posn, capture=Capture.ONLY, stop_short=True)
+        moves += \
+        movescan(x_start, y_start, direction, 0, posn, capture=False, stop_short=True)
     return moves
 
 
 def find_moves(posn):
+    '''
+    Find all pieces for the color on move, then find all moves for those pieces.
+
+    :param posn: Current board state.
+    :returns: Move list containing all possible moves for color on move.
+    '''
     locations = []
     for i in range(len(posn.board)):
         for j in range(len(posn.board[i])):
@@ -95,4 +132,3 @@ def find_moves(posn):
     for location in locations:
         moves += movelist(location[0], location[1], posn)
     return moves
-
--- pychess/players.py
+++ pychess/players.py
@@ -1,32 +1,49 @@
-import move_gen
+'''
+Contains the various kinds of players.
+'''
+
 import random as r
+
+import move_gen
 import representation
 from representation import Posn
 
 class HumanPlayer(object):
-    
+    '''
+    Object to represent a human player.
+    '''
     def __init__(self, color):
         self.posn = Posn(color)
 
-    
+
     def make_move(self):
+        '''
+        Get a move from input and execute it.
+        '''
         move = representation.decode(input())
         self.posn.make_move(move)
         return move.toStr(), move.win
 
-    
+
     def get_move(self, move):
+        '''
+        Update position representation with received move.
+        '''
         move = representation.decode(move)
         self.posn.make_move(move)
 
 
 class RandomPlayer(object):
-
-    
+    '''
+    Player that chooses moves randomly.
+    '''
     def __init__(self, color):
         self.posn = Posn(color)
 
     def make_move(self):
+        '''
+        Choose move randomly and execute it.
+        '''
         moves = move_gen.find_moves(self.posn)
         move = moves[r.randint(0, len(moves))-1]
         self.posn.make_move(move)
@@ -34,17 +51,25 @@ class RandomPlayer(object):
 
 
     def get_move(self, move):
-        move = representation.decode(move)        
+        '''
+        Update position representation with received move.
+        '''
+        move = representation.decode(move)
         self.posn.make_move(move)
 
 
 class HeuristicPlayer(object):
-
+    '''
+    Player that chooses highest-scoring move.
+    '''
     def __init__(self, color):
         self.posn = Posn(color)
 
 
     def make_move(self):
+        '''
+        Generate all moves and choose highest-scoring.
+        '''
         moves = move_gen.find_moves(self.posn)
         scored_moves = []
         for move in moves:
@@ -58,37 +83,53 @@ class HeuristicPlayer(object):
 
 
     def get_move(self, move):
+        '''
+        Update position representation with received move.
+        '''
         move = representation.decode(move)
         self.posn.make_move(move)
 
 
 class NegamaxPlayer:
-
+    '''
+    Choose move based on negamax algorithm. Requires depth to search to.
+    '''
     def __init__(self, color, depth):
         self.posn = Posn(color)
         self.depth = depth
 
 
     def make_move(self):
+        '''
+        Run negamax algorithm to choose move.
+        '''
         moves = move_gen.find_moves(self.posn)
         move = self.posn.negamax_move(self.depth, moves)
         self.posn.make_move(move)
         return move.toStr(), move.win
 
-    
+
     def get_move(self, move):
+        '''
+        Update position representation with received move.
+        '''
         move = representation.decode(move)
         self.posn.make_move(move)
 
 
 class AlphaBetaPlayer:
-
+    '''
+    Player that chooses move based on alpha-beta algorithm.
+    '''
     def __init__(self, color, depth):
         self.posn = Posn(color)
         self.depth = depth
 
 
     def make_move(self):
+        '''
+        Run alpha-beta algorithm to choose move.
+        '''
         moves = move_gen.find_moves(self.posn)
         move = self.posn.alpha_beta_move(self.depth, moves)
         self.posn.make_move(move)
@@ -96,12 +137,17 @@ class AlphaBetaPlayer:
 
 
     def get_move(self, move):
+        '''
+        Update position representation with received move.
+        '''
         move = representation.decode(move)
         self.posn.make_move(move)
 
 
 class IDPlayer:
-
+    '''
+    Player that chooses move based on alpha-beta using iterative deepening.
+    '''
     def __init__(self, color, limit):
         self.posn = Posn(color)
         self.use_adjustment = False
@@ -111,20 +157,30 @@ class IDPlayer:
 
 
     def make_move(self):
+        '''
+        Choose move using iteratively deepened alpha-beta.
+        '''
         moves = move_gen.find_moves(self.posn)
         if self.use_adjustment is True:
             self.limit = self.determine_time()
-        move = self.posn.id(self.limit, moves)
+        move = self.posn.iterative_deepening(self.limit, moves)
         self.posn.make_move(move)
         return move.toStr(), move.win
 
 
     def get_move(self, move):
+        '''
+        Update position representation with received move.
+        '''
         move = representation.decode(move)
         self.posn.make_move(move)
 
 
     def determine_time(self):
+        '''
+        Adjust time spent doing iterative deepening depending on how long the
+        game has lastest so far.
+        '''
         if self.posn.ply // 2 < 5:
             return 1
         elif self.posn.ply // 2 < 10:
