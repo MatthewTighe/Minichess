88bcfb7f420eee7b9f0526bb2c77b9a613e0348e
MatthewTighe <matthewdtighe@gmail.com>
2018-02-05 16:22:22 -0800

    Made server loop endlessly but timeout after 10 seconds.

--- pychess/server.py
+++ pychess/server.py
@@ -9,16 +9,19 @@ AI_PLAYER = IDPlayer('B', 7)
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.bind((HOST, PORT))
     sock.listen(1)
-    CONN, ADDR = sock.accept()
-    with CONN:
-        while True:
-            DATA = CONN.recv(1024)
-            if not DATA:
-                break
-            # TODO add more error checking for properly received move
-            move = DATA.decode('utf-8')
-            AI_PLAYER.get_move(move)
-            print(AI_PLAYER.posn)
-            AI_PLAYER.make_move()
-            CONN.sendall(AI_PLAYER.posn.__str__().encode('utf-8'))
-            print(AI_PLAYER.posn)
+    sock.settimeout(10)
+    while True:
+        try:
+            CONN, ADDR = sock.accept()
+        except Exception as e:
+            break
+        DATA = CONN.recv(1024)
+        if not DATA:
+            break
+        # TODO add more error checking for properly received move
+        move = DATA.decode('utf-8')
+        AI_PLAYER.get_move(move)
+        print(AI_PLAYER.posn)
+        AI_PLAYER.make_move()
+        CONN.sendall(AI_PLAYER.posn.__str__().encode('utf-8'))
+        print(AI_PLAYER.posn)
